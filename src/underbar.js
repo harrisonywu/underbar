f65767a23625bcb061fe483afae2db565754f31e
Harrison Wu <harrisonywu@gmail.com>
2019-01-28 18:20:51 -0800

    Pass all tests up to _.delay function

--- src/underbar.js
+++ src/underbar.js
@@ -345,6 +345,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var actualArgs = Array.prototype.slice.call(arguments,2)
+    setTimeout(function() {
+      return func.apply(this, actualArgs);
+    }, wait);
   };
 
 
